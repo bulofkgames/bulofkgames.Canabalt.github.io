c956c5555962e56edd172ae38dbc92b8756fd6db
bulofkgames <leodiyu@gmail.com>
2025-10-29 23:07:10 -0300

    delet

--- source/LeaderboardPrefs.hx
+++
@@ -1,9 +0,0 @@
-package;
-
-import io.newgrounds.components.ScoreBoardComponent.Period;
-
-class LeaderboardPrefs
-{
-	public static var period:Period = ALL;
-	public static var FRIENDS_ONLY:Bool = false;
-}
